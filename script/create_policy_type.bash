8f06c6424f60ba00206dc26623f98665548f0956
zanattabruno <gkbsa2504@gmail.com>
2024-02-13 19:38:49 +0000

    Initial commit

---
+++ script/create_policy_type.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -v -X PUT http://service-ricplt-a1mediator-http.ricplt.svc.cluster.local:10000/a1-p/policytypes/3 \
+-H "Content-Type: application/json" \
+-d @gNBEnergySaverSchema.json
---
+++ script/delete_policy_type.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define the policy ID to delete
+POLICY_ID=3
+
+# Execute the curl command to delete the policy
+curl -v -X DELETE http://service-ricplt-a1mediator-http.ricplt.svc.cluster.local:10000/a1-p/policytypes/${POLICY_ID}
