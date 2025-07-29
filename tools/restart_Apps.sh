c561e1be2298af3cf715310bf57bd087f3ea2f1f
zanattabruno <gkbsa2504@gmail.com>
2025-07-29 03:17:57 +0000

    Increase sleep duration in restart script: extend wait time to 10 seconds for better stability during app restarts.

--- tools/restart_Apps.sh
+++ tools/restart_Apps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Restarting Apps." &&
 kubectl rollout restart deployment e2sim-e2sim-helm -n ricplt &&
-sleep 5 &&
+sleep 10 &&
 kubectl rollout restart deployment ricxapp-bouncer-xapp -n ricxapp &&
-sleep 5 &&
+&&
 kubectl rollout restart deployment ricxapp-debugger-xapp -n ricxapp &&
 printf "Apps restarted successfully.\n" 
